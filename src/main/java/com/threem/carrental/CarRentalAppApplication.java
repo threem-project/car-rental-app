73d79d312adeb02171c03250f9098ae7d9dbc0eb
juszczak.marek@gmail.com <juszczak.marek@gmail.com>
2018-04-25 21:03:10 +0200

    Initial commit with Spring Boot applied

---
+++ src/main/java/com/threem/carrental/CarRentalAppApplication.java
@@ -0,0 +1,12 @@
+package com.threem.carrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalAppApplication.class, args);
+	}
+}
