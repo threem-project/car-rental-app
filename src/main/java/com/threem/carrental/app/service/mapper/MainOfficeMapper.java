3b5b8662b792bf3d0515ab914bedd6703c461849
miszalemko <lemkomisza@gmail.com>
2018-05-27 14:21:12 +0200

    save commit

---
+++ src/main/java/com/threem/carrental/app/service/mapper/MainOfficeMapper.java
@@ -0,0 +1,14 @@
+package com.threem.carrental.app.service.mapper;
+
+import com.threem.carrental.app.model.entity.MainOfficeEntity;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author misza_lemko on 27.05.2018
+ * @project car-rental-app
+ */
+@Service
+public class MainOfficeMapper {
+
+    //public MainOfficeEntity toMainOfficeEntity()
+}
