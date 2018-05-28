4c50f8219029f6e0d0435c01b176828864ed4e88
miszalemko <lemkomisza@gmail.com>
2018-05-28 14:02:52 +0200

    add unit tests for AddressBranchMapper and MainOfficeMapper

--- src/test/java/com/threem/carrental/app/service/mapper/AddressBranchMapperTest.java
+++ src/test/java/com/threem/carrental/app/service/mapper/AddressBranchMapperTest.java
@@ -1,5 +1,9 @@
 package com.threem.carrental.app.service.mapper;
 
+import com.threem.carrental.app.model.dto.AddressBranchDto;
+import com.threem.carrental.app.model.entity.AddressBranchEntity;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,8 +13,61 @@ import static org.junit.Assert.*;
  * @project car-rental-app
  */
 public class AddressBranchMapperTest {
-    //@Test
-    //public void shouldMapAddressBranchDtoToAddressBranch() throws Exception {
-    //}
 
+    private AddressBranchMapper addressBranchMapper;
+
+    @Before
+    public void setup() throws Exception {
+        addressBranchMapper = new AddressBranchMapper();
+    }
+
+    @Test
+    public void shouldMapAddressBranchDtoToAddressBranchEntity() throws Exception {
+        //given
+        AddressBranchDto addressBranchDto = AddressBranchDto.builder()
+                .id(1L)
+                .city("Warsaw")
+                .street("Towarowa")
+                .building("20/10")
+                .zipCode("02-495")
+                .country("Poland")
+                .phone("111-222-333")
+                .build();
+        //when
+        AddressBranchEntity addressBranchEntity = addressBranchMapper.toAddressBranchEntity(addressBranchDto);
+        //then
+        Assertions.assertThat(addressBranchEntity)
+                .hasFieldOrPropertyWithValue("id", addressBranchDto.getId())
+                .hasFieldOrPropertyWithValue("city", addressBranchDto.getCity())
+                .hasFieldOrPropertyWithValue("street", addressBranchDto.getStreet())
+                .hasFieldOrPropertyWithValue("building", addressBranchDto.getBuilding())
+                .hasFieldOrPropertyWithValue("zipCode", addressBranchDto.getZipCode())
+                .hasFieldOrPropertyWithValue("country", addressBranchDto.getCountry())
+                .hasFieldOrPropertyWithValue("phone", addressBranchDto.getPhone());
+    }
+
+    @Test
+    public void shouldMapAddressBranchEntityToAddressBranchDto() throws Exception {
+        //given
+        AddressBranchEntity addressBranchEntity = AddressBranchEntity.builder()
+                .id(1L)
+                .city("Warsaw")
+                .street("Towarowa")
+                .building("20/10")
+                .zipCode("02-495")
+                .country("Poland")
+                .phone("111-222-333")
+                .build();
+        //when
+        AddressBranchDto addressBranchDto = addressBranchMapper.toAddressBranchDto(addressBranchEntity);
+        //then
+        Assertions.assertThat(addressBranchDto)
+                .hasFieldOrPropertyWithValue("id", addressBranchEntity.getId())
+                .hasFieldOrPropertyWithValue("city", addressBranchEntity.getCity())
+                .hasFieldOrPropertyWithValue("street", addressBranchEntity.getStreet())
+                .hasFieldOrPropertyWithValue("building", addressBranchEntity.getBuilding())
+                .hasFieldOrPropertyWithValue("zipCode", addressBranchEntity.getZipCode())
+                .hasFieldOrPropertyWithValue("country", addressBranchEntity.getCountry())
+                .hasFieldOrPropertyWithValue("phone", addressBranchEntity.getPhone());
+    }
 }
---
+++ src/test/java/com/threem/carrental/app/service/mapper/BranchMapperTest.java
@@ -0,0 +1,16 @@
+package com.threem.carrental.app.service.mapper;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author misza_lemko on 28.05.2018
+ * @project car-rental-app
+ */
+public class BranchMapperTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+}
